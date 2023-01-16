dee053c1cc9878ddb553689064d765f65381870e
rica213 <25f5k252l@mozmail.com>
2023-01-17 00:30:18 +0300

    Refactor the remove and add  book functions

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ const booksContainer = document.querySelector('.books-container');
 const newTitle = document.getElementById('new-title');
 const newAuthor = document.getElementById('new-author');
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
 /* local storage */
@@ -29,33 +31,29 @@ const retrieve = () => {
 };
 
 const createBookList = () => {
-  books.forEach((book) => {
+  books.forEach((book, index) => {
     const li = document.createElement('li');
     li.className = 'book-item';
     li.innerHTML = `
       <p class="new-title">${book.title}</p>
       <p class="new-author">${book.author}</p>
-      <button class="remove-btn" type="button">Remove</button>
+      <button class="remove-btn" id="${index}" type="button">Remove</button>
       <hr>
     `;
     booksContainer.appendChild(li);
   });
 }
 
-window.addEventListener('load', () => {
-  retrieve();
-  createBookList();
-
+const remove = function () {
   const removeArr = document.querySelectorAll('.remove-btn');
   removeArr.forEach((remove, index) => {
     remove.addEventListener('click', () => {
       books.splice(index, 1);
       booksContainer.innerHTML = '';
-
       createBookList();
     });
   });
-});
+} 
 
 const add = function () {
   const newBook = Object.create(Book);
@@ -69,3 +67,16 @@ const add = function () {
 
 const addBook = document.querySelector('.add-btn');
 addBook.addEventListener('click', add);
+
+window.addEventListener('load', () => {
+  retrieve();
+  createBookList();
+});
+
+booksContainer.addEventListener('click', ((e) => {
+  books.splice(e.target.id, 1);
+  e.target.parentElement.remove();
+
+  localStorage.setItem('books', JSON.stringify(books));
+}));
+
